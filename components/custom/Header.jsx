3b0871699d53bb6591f62be83aba707c24927741
Nishant <nishantrathod484@gmail.com.com>
2025-04-14 00:08:43 +0530

    header

--- components/custom/Header.jsx
+++ components/custom/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from 'react';
 import Image from 'next/image';
-import { Button } from '../ui/button';
 import { UserDetailContext } from '@/contex/UserDetailContext';
 import SignInDialog from './SignInDialog';
 
@@ -8,31 +7,10 @@ function Header() {
     const { userDetail } = useContext(UserDetailContext);
     const [openDialog, setOpenDialog] = useState(false);
 
-    const handleSignInClick = () => {
-        setOpenDialog(true);
-    };
-
     return (
         <div className='p-4 flex justify-between items-center'>
             <Image src={"/download (2).png"} alt="logo" width={70} height={70} />
-            {!userDetail?.name && (
-                <div className='flex gap-5'>
-                    <Button 
-                        variant="ghost" 
-                        onClick={handleSignInClick}
-                        className="text-white hover:text-gray-200"
-                    >
-                        Sign In
-                    </Button>
-                    <Button
-                        variant="outline"
-                        onClick={handleSignInClick}
-                        className="bg-blue-600 hover:bg-blue-700 text-white"
-                    >
-                        Get Started
-                    </Button>
-                </div>
-            )}
+            {/* Buttons removed */}
             <SignInDialog 
                 openDialog={openDialog} 
                 closeDialog={(v) => setOpenDialog(v)} 
